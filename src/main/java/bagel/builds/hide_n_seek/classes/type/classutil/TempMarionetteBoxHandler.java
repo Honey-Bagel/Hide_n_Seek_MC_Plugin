6772eae18724c831b1385215e34e767b690c83f9
Honey-Bagel <82608840+Honey-Bagel@users.noreply.github.com>
2023-11-06 01:00:59 -0800

    commit3

--- src/main/java/bagel/builds/hide_n_seek/classes/type/classutil/TempMarionetteBoxHandler.java
+++ src/main/java/bagel/builds/hide_n_seek/classes/type/classutil/TempMarionetteBoxHandler.java
@@ -2,32 +2,21 @@ package bagel.builds.hide_n_seek.classes.type.classutil;
 
 import bagel.builds.hide_n_seek.Main;
 import bagel.builds.hide_n_seek.manager.GameManager;
-import bagel.builds.hide_n_seek.manager.JukeBoxFileManager;
 import org.bukkit.*;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
-import org.bukkit.block.TileState;
-import org.bukkit.block.data.type.Jukebox;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BlockDataMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.io.File;
 import java.util.List;
-import java.util.Objects;
 
 public class TempMarionetteBoxHandler implements Listener {
 
     private Main main;
     private GameManager gameManager;
     private ItemStack musicBox;
-    private FileConfiguration musicboxes;
-    private JukeBoxFileManager locmanager;
     private List<Location> locations;
 
 
@@ -41,39 +30,17 @@ public class TempMarionetteBoxHandler implements Listener {
         boxMeta.setLocalizedName("marionette music box maker");
         musicBox.setItemMeta(boxMeta);
 
-        this.locmanager = new JukeBoxFileManager(main);
-        this.musicboxes = locmanager.getData();
     }
 
     @EventHandler
     public void onBoxPlace(BlockPlaceEvent e) {
         Player player = e.getPlayer();
-        int count = musicboxes.getConfigurationSection("locations").getKeys(false).size();
         if(e.getItemInHand().equals(musicBox) && e.getBlockPlaced().getType().equals(Material.JUKEBOX)) {
-            Location loc = e.getBlockPlaced().getLocation();
-
-            musicboxes.set("locations.musicbox-" + count + ".x.", loc.getBlockX());
-            musicboxes.set("locations.musicbox-" + count + ".y.", loc.getBlockY());
-            musicboxes.set("locations.musicbox-" + count + ".z.", loc.getBlockZ());
-            musicboxes.set("locations.musicbox-" + count + ".world.", loc.getWorld());
-
-            locmanager.saveConfig();
+            //save block location to yml file
         }
     }
 
-    @EventHandler
-    public void onEnable() {
-        locmanager.load();
-
-        for(String string : Objects.requireNonNull(musicboxes.getConfigurationSection("locations.").getKeys(false))) {
-            locations.add(new Location(
-                    Bukkit.createWorld(new WorldCreator(musicboxes.getString("location." + string + ".world"))),
-                    musicboxes.getDouble("locations." + string + ".x"),
-                    musicboxes.getDouble("locations." + string + ".y"),
-                    musicboxes.getDouble("locations." + string + ".z")));
-        }
-        System.out.println(locations.toArray().toString());
-    }
+    //on start add locations in yml file to a list to be used
 
     public ItemStack getMusicBox() { return musicBox; }
 
