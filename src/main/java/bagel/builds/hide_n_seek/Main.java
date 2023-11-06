00d7242d6029d2fd99a720286695d7c2535b5ee8
Honey-Bagel <82608840+Honey-Bagel@users.noreply.github.com>
2023-11-06 00:53:57 -0800

    commit2

--- src/main/java/bagel/builds/hide_n_seek/Main.java
+++ src/main/java/bagel/builds/hide_n_seek/Main.java
@@ -1,17 +1,25 @@
 package bagel.builds.hide_n_seek;
 
-import bagel.builds.hide_n_seek.commands.*;
-import bagel.builds.hide_n_seek.handlers.FnafGame.Features.VentHandler;
-import bagel.builds.hide_n_seek.handlers.misc.*;
+import bagel.builds.hide_n_seek.classes.type.classutil.TempMarionetteBoxHandler;
+import bagel.builds.hide_n_seek.command.*;
+import bagel.builds.hide_n_seek.listener.GameListener;
+import bagel.builds.hide_n_seek.listener.misc.*;
+import bagel.builds.hide_n_seek.manager.GameManager;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 
 
 public final class Main extends JavaPlugin {
 
+    private GameManager gameManager;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        new CarryHandler(this);
         new IronTDHandler(this);
         new FallingBlockHandler(this);
         new CarryHandler(this);
@@ -20,13 +28,19 @@ public final class Main extends JavaPlugin {
         new GuiListener(this);
         new VentHandler(this);
 
+        gameManager = new GameManager(this);
+
         getCommand("nightvision").setExecutor(new NightVision());
         getCommand("sit").setExecutor(new SitCommand());
         getCommand("speed").setExecutor(new SpeedCommand());
         getCommand("speed").setTabCompleter(new SpeedTabCompleter());
         getCommand("hat").setExecutor(new HatCommand());
         getCommand("hat").setTabCompleter(new HatTabCompleter());
-        getCommand("gui").setExecutor(new GuiCommand());
+        getCommand("gui").setExecutor(new GuiCommand(this));
+        getCommand("vent").setExecutor(new TestCommand(this));
+
+        Bukkit.getPluginManager().registerEvents(new GameListener(this, gameManager), this);
+        Bukkit.getPluginManager().registerEvents(new TempMarionetteBoxHandler(this, gameManager), this);
 
     }
 
@@ -35,4 +49,14 @@ public final class Main extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    private void initiateFile(String name) throws Exception {
+        File file = new File(getDataFolder(), name);
+        if(!file.exists()) {
+            file.createNewFile();
+        }
+        YamlConfiguration modifyFile = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public GameManager getGameManager() { return gameManager; }
 }
